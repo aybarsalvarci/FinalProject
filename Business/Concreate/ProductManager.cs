1ca24869381b1b286a7e0d45a1129929aac32603
aybarsalvarci <aybarsalvarci44@gmail.com>
2021-02-28 14:41:40 +0300

    9.Gün Sonu

--- Business/Concreate/ProductManager.cs
+++ Business/Concreate/ProductManager.cs
@@ -1,6 +1,7 @@
 ﻿using Business.Abstract;
 using DataAccess.Abstract;
 using Entities.Concreate;
+using Entities.DTOs;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -21,6 +22,21 @@ namespace Business.Concreate
         {
             //İş kodları
             return _productDal.GetAll();
+        }
+
+        public List<Product> GetAllByCategoryId(int id)
+        {
+            return _productDal.GetAll(p=>p.CategoryId==id);
+        }
+
+        public List<Product> GetByUnitPrice(decimal min, decimal max)
+        {
+            return _productDal.GetAll(p=>p.UnitPrice >= min && p.UnitPrice <= max);
+        }
+
+        public List<ProductDetailDto> GetProductDetails()
+        {
+            return _productDal.GetProductDetails();
         }
     }
 }
